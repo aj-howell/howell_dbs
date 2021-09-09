7c560062f9c4f3606f00338b7ce215838f791a38
aj.howell <aj.howell@centre.edu>
2021-09-09 21:17:54 +0000

    attempt on using sql script

---
+++ tutorial.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE pets;
+
+SHOW DATABASES;
+
+USE pets;
+
+CREATE TABLE cats
+(
+id INT unsigned NOT NULL AUTO_INCREMENT,
+name VARCHAR(100) NOT NULL,
+owner VARCHAR(100) NOT NULL,
+birth DATE NOT NULL,
+PRIMARY KEY (id)
+
+
+);
+
+SHOW TABLES;
+
+DESCRIBE cats;
+
+INSERT INTO cats ( name, owner, birth) VALUES
+('Sandy', 'Lennon', '2015-01-03'),
+('Cookie', 'Casey', '2013-11-13'),
+('Charlie', 'River', '2016-05-21');
+
+SELECT * FROM cats;
+
+SELECT name FROM cats WHERE owner = 'Casey';
+
+DELETE FROM cats WHERE name = 'Cookie';
+
+SELECT * FROM cats;
+
+ALTER TABLE cats ADD gender CHAR(1) AFTER name;
+
+DESCRIBE cats;
+
+SHOW CREATE TABLE cats\G;
+
+ALTER TABLE cats DROP gender;
+
+DESCRIBE cats;
