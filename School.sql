656f7dbaa439ea442fd32df627be8499d86dd1eb
aj.howell <aj.howell@centre.edu>
2021-09-10 01:46:43 +0000

    updated

--- School.sql
+++ School.sql
@@ -9,12 +9,12 @@ CREATE TABLE instructors
 insturctor_id INT NOT NULL AUTO_INCREMENT,
 inst_first_name VARCHAR(20),
 inst_last_name VARCHAR(20),
-campus_phone VARCHAR(20),
+campus_phone CHAR(20),
 PRIMARY KEY (insturctor_id)
 
 );
 
-INSERT INTO instructors (inst_first_name, inst_last_name, campus_phone) VALUES
+INSERT INTO instructors (inst_first_name, inst_last_name, campus_phone ) VALUES
 ('Kira', 'Bently', '363-9948'),
 ('Timothy', 'Ennis', '527-4992'),
 ('Shannon', 'Black', '322-5992'),
