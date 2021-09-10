d6df72351bf92dad0001dd5fbb29c7c1922322e3
aj.howell <aj.howell@centre.edu>
2021-09-10 01:28:05 +0000

    had two existing dbs in one script but I found out that you would need to make a list for that

--- tutorial.sql
+++ tutorial.sql
@@ -43,26 +43,3 @@ ALTER TABLE cats DROP gender;
 
 DESCRIBE cats;
 
-CREATE DATABASE school;
-
-SHOW DATABASE;
-
-USE school;
-
-CREATE TABLE instructors
-(
-insturctor_id INT NOT NULL AUTO_INCREMENT,
-inst_first_name VARCHAR(20) NOT NULL,
-inst_last_name VARCHAR(20) NOT NULL,
-campus_phone INT NOT NULL
-PRIMARY KEY (insturctor_id)
-
-);
-
-INSERT INTO instructors (inst_first_name, inst_last_name, campus_phone) VALUES
-('Kira', 'Bently', '363-9948'),
-('Timothy', 'Ennis', '527-4992'),
-('Shannon', 'Black', '322-5992'),
-('Estela', 'Rosales', '322-6992');
-
-SELECT * FROM instructors;
