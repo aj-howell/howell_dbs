476e1083c6e4a704414e0fe76971d9dcdbe1371c
aj.howell <aj.howell@centre.edu>
2021-09-10 01:18:37 +0000

    finished tutorial but need to make comments

--- tutorial.sql
+++ tutorial.sql
@@ -1,3 +1,4 @@
+/*going through the steps of the tutorial*/
 CREATE DATABASE pets;
 
 SHOW DATABASES;
@@ -41,3 +42,21 @@ SHOW CREATE TABLE cats\G;
 ALTER TABLE cats DROP gender;
 
 DESCRIBE cats;
+
+CREATE TABLE instructors
+(
+insturctor_id INT NOT NULL AUTO_INCREMENT,
+inst_first_name VARCHAR(20) NOT NULL,
+inst_last_name VARCHAR(20) NOT NULL,
+campus_phone INT NOT NULL
+PRIMARY KEY (insturctor_id)
+
+);
+
+INSERT INTO instructors (inst_first_name, inst_last_name, campus_phone) VALUES
+('Kira', 'Bently', '363-9948'),
+('Timothy', 'Ennis', '527-4992'),
+('Shannon', 'Black', '322-5992'),
+('Estela', 'Rosales', '322-6992');
+
+SELECT * FROM instructors;
